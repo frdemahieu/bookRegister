c2e29bfed81356784d77938a877f7d0d19749173
frdemahieu <frdemahieu@hotmail.com>
2018-04-09 22:18:02 -0400

    addcatcher

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ var url = 'mongodb+srv://localhost:27017/test';
 var bodyParser = require('body-parser');
 
 
-mongoose.connect(url);	
+mongoose.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
 
 
 app.use(express.static(__dirname + '/vue')); 
